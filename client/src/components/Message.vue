2dbb6ef6f81f1518f86aa50ebe95a23ba959c5e5
Elmo V <72126010+ElmoVah@users.noreply.github.com>
2021-11-27 15:19:46 +0200

    Message komponentin prototyyppi

---
+++ client/src/components/Message.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="message">
+			<p>Id: {{message_data.writer_id}}, Date: {{message_data.posted_time}} </p>
+    <p>{{message_data.content}}</p>
+			<p>Score: {{message_data.score}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+	name: 'Message',
+	props: {
+		message_data: Object
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
