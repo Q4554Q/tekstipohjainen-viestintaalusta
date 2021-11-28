b0a871c3820a8dfd00681b86d734aa42b536514e
samihuo <samihuoppila@gmail.com>
2021-11-28 13:57:59 +0200

    NewMessage lisätty

---
+++ client/src/components/NewMessage.vue
@@ -0,0 +1,32 @@
+<template>
+<div id="newmessage">
+		<div id="newMessageArea" class="container p-3 my-3 border">
+			<b-form-textarea
+				v-model="message"
+				size="lg"
+				placeholder="Kirjoita viesti"
+				rows="5"
+				no-resize
+				maxlength="350"
+			></b-form-textarea>
+			<button @click="handleNewMessage">Lähetä viesti</button>
+		</div>
+</div>
+</template>
+
+<script>
+export default {
+	name: 'NewMessage.vue',
+	methods: {
+		handleNewMessage () {
+			// otetaan message ja luodaan uusi Message-olio
+		}
+	}
+}
+</script>
+
+<style scoped>
+#newMessageArea {
+	max-width: 800px;
+}
+</style>
