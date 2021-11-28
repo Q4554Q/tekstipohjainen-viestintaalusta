420f7009784fdc5ba3c5982305d0e9ff4e7c2860
samihuo <samihuoppila@gmail.com>
2021-11-28 13:56:13 +0200

    Threadiin päivitys, vastaanottaa messages Arrayn

--- client/src/components/Thread.vue
+++ client/src/components/Thread.vue
@@ -1,54 +1,23 @@
 <template>
-  <div id="thread">
-    <table>
-      <thead>
-      <tr>
-        <th>Viestiketju x</th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr v-for="message in messages" :key="message.posted_time">
-        <td>
-          <message v-bind:message_data=message></message>
-        </td>
-      </tr>
-      </tbody>
-    </table>
-  </div>
+	<div id="thread">
+		<message v-for="message in messages" :key="message.posted_time" v-bind:message_data="message"></message>
+		<NewMessage></NewMessage>
+	</div>
 </template>
 
 <script>
 
 import Message from './Message'
+import NewMessage from './NewMessage'
 
 export default {
 	name: 'Thread',
 	components: {
-		Message
+		Message,
+		NewMessage
 	},
-	data () {
-		return {
-			messages: [
-				{
-					writer_id: 1,
-					content: 'Äiti mä oon internetissä',
-					score: 12,
-					posted_time: '13.12.2021'
-				},
-				{
-					writer_id: 2,
-					content: 'EBIN TROLORLROR',
-					score: -3,
-					posted_time: '13.12.2021'
-				},
-				{
-					writer_id: 666,
-					content: '@2 on apina!',
-					score: 4,
-					posted_time: '13.12.2021'
-				}
-			]
-		}
+	props: {
+		messages: Array
 	}
 }
 
