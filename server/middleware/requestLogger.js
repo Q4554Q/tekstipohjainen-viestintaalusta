bcfa962dabcc16188ec917d616b351b2d74cd353
Saku Myyryläinen <saku.myyrylainen@gmail.com>
2021-12-04 22:29:47 +0200

    Salasanojen piilotus logiviesteistä

--- server/middleware/requestLogger.js
+++ server/middleware/requestLogger.js
@@ -3,9 +3,25 @@ const logger = require('../utils/logger')
 const requestLogger = (req, res, next) => {
 	logger.info('Method:', req.method)
 	logger.info('Path:  ', req.path)
-	logger.info('Body:  ', req.body)
+	logger.info('Body:  ', hidePasswords(req.body))
 	logger.info('---')
 	next()
+}
+
+const hidePasswords = (body) => {
+	let bodyWithHiddenPasswords = {}
+	const hiddenProperties = [
+		'password',
+		'passwordhash'
+	]
+
+	for (const property in body) {
+		bodyWithHiddenPasswords[property] = hiddenProperties.includes(property.toLowerCase())
+			? '******'
+			: body[property]
+	}
+
+	return bodyWithHiddenPasswords
 }
 
 module.exports = requestLogger
