42d38ac2ae9254c44a664b925efa716766e430be
Elmo V <72126010+ElmoVah@users.noreply.github.com>
2021-12-05 13:06:00 +0200

    TopBarin nuoli vaihdettu koti kuvakkeeseen

--- client/src/components/TopBar.vue
+++ client/src/components/TopBar.vue
@@ -1,23 +1,23 @@
 <template>
 	<div id="top-bar" class="container p-3 rounded-bottom bg-primary text-white">
 		<div class="d-flex justify-content-between">
-				<button type="button" class="btn bg-white btn-lg text-primary" @click="$emit('return-clicked')"><BackIcon id="back-icon"/></button>
+				<button type="button" class="btn bg-white btn-lg text-primary" @click="$emit('return-clicked')"><HomeIcon id="home-icon"/></button>
 				<button type="button" class="btn bg-white btn-lg text-primary" @click="$emit('newthread-clicked')">New thread <PenIcon id="pen-icon"/></button>
-				<button type="button" class="btn bg-white btn-lg text-primary" @click="$emit('profile-clicked')"><ProfileIcon id="back-icon"/></button>
+				<button type="button" class="btn bg-white btn-lg text-primary" @click="$emit('profile-clicked')"><ProfileIcon id="profile-icon"/></button>
 		</div>
 	</div>
 </template>
 
 <script>
 import PenIcon from '../assets/PenIcon'
-import BackIcon from '../assets/BackIcon'
+import HomeIcon from '../assets/HomeIcon'
 import ProfileIcon from '../assets/ProfileIcon'
 
 export default {
 	name: 'TopBar',
 	components: {
 		ProfileIcon,
-		BackIcon,
+		HomeIcon,
 		PenIcon
 	}
 }
@@ -32,7 +32,7 @@ export default {
 	height: 1.2em;
 	width: 1.2em;
 }
-#back-icon{
+#home-icon, #profile-icon{
 	fill: #4285F4;
 	height: 1.5em;
 	width: 1.5em;
