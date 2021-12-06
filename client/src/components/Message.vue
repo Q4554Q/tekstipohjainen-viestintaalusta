82ec26dafc27382c18fe2b5d2205d5e6c89198ca
Elmo V <72126010+ElmoVah@users.noreply.github.com>
2021-12-06 13:45:38 +0200

    Message komponentin visualista kaunistelua

--- client/src/components/Message.vue
+++ client/src/components/Message.vue
@@ -1,24 +1,37 @@
 <template>
-  <div id="message" class="container p-3 my-3 border rounded" @click="$emit('message-clicked')">
-
-			<div class="d-flex align-items-start">
-				<span class="badge rounded-pill bg-primary mx-3">{{ message_data.writerId }}</span>
-				<Clock id="clock-icon"/><small class="text-secondary"> {{ message_data.postedTime }}</small>
-			</div>
-
-			<div class="row">
-				<div class="col-sm-10 px-5 py-3 text-break">
+	<div id="message" class="container px-3 my-3 border rounded" @click="$emit('message-clicked')">
+		<div class="row">
+			<div class="col-sm-10 px-5 pt-4 text-break">
+				<div class="row">
+					<div class="d-flex align-items-start mb-1">
+						<span class="badge rounded-pill bg-primary">{{ message_data.writerId }}</span>
+						<Clock id="clock-icon"/>
+						<small class="text-secondary"> {{ message_data.postedTime }}</small>
+					</div>
+				</div>
+				<div class="row p-3">
 					{{ message_data.content }}
 				</div>
-				<div class="col-sm-2 text-center">
-					<button type="button" class="btn btn-primary btn-sm" @click="handleUpvote"><UpvoteIcon id="vote-icons"/></button>
-					<br>
-					<b>{{ message_data.score }}</b>
-					<br>
-					<button type="button" class="btn btn-primary btn-sm" @click="handleDownvote"><DownvoteIcon id="vote-icons"/></button>
+			</div>
+
+			<div class="col-sm-2 text-center d-flex flex-column m-auto">
+				<div class="row">
+					<button type="button" class="btn btn-xs" @click="handleUpvote">
+						<UpvoteIcon id="upvote"/>
+					</button>
+				</div>
+				<div class="row">
+					<span id="score" class="fw-bold fs-4">{{ message_data.score }}</span>
+				</div>
+				<div class="row">
+					<button type="button" class="btn btn-xs" @click="handleDownvote">
+						<DownvoteIcon id="downvote"/>
+					</button>
 				</div>
 			</div>
-  </div>
+
+		</div>
+	</div>
 </template>
 
 <script>
@@ -51,7 +64,9 @@ export default {
 					headers: {
 						Authorization: `bearer ${window.accessToken}`
 					}
-				}).then(response => { this.message_data.score = response.data.score })
+				}).then(response => {
+					this.message_data.score = response.data.score
+				})
 
 				this.error = 0
 			} catch (error) {
@@ -73,7 +88,9 @@ export default {
 					headers: {
 						Authorization: `bearer ${window.accessToken}`
 					}
-				}).then(response => { this.message_data.score = response.data.score })
+				}).then(response => {
+					this.message_data.score = response.data.score
+				})
 
 				this.error = 0
 			} catch (error) {
@@ -91,18 +108,23 @@ export default {
 </script>
 
 <style scoped>
-#message{
+#message {
 	width: 800px;
 }
 
-#clock-icon{
+#clock-icon {
 	margin-top: 0.3em;
+	margin-right: 0.5em;
+	margin-left: 0.5em;
 	width: 0.7em;
 	height: 0.7em;
 	fill: #777;
 }
 
-#vote-icons{
-	fill: white;
+#upvote, #downvote {
+	fill: #4285F4;
+	width: 2.5em;
+	height: 2.5em;
 }
+
 </style>
