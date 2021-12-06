360d9636a6f4906461b7d321cec4642d5ee37654
Elmo V <72126010+ElmoVah@users.noreply.github.com>
2021-12-06 17:42:37 +0200

    ThreadListssä käyttäjä näkee pienen tähden aloittamiensa threadien vasemmassa yläkulmassa

--- client/src/components/OpeningMessage.vue
+++ client/src/components/OpeningMessage.vue
@@ -1,12 +1,14 @@
 <template>
 	<div id="message" class="container p-3 my-3 rounded-3" @click="$emit('message-clicked')">
+		<StarIcon v-if="writerIdInThread === messageData.writerId" id="star-icon"/>
 
-		<div class="d-flex align-items-start mx-4">
-			<Clock id="clock-icon"/><small class="text-secondary"> {{ messageData.postedTime }}</small>
+		<div class="d-flex align-items-start mx-4 my-1">
+			<Clock id="clock-icon"/>
+			<small class="text-secondary"> {{ messageData.postedTime }}</small>
 		</div>
 
 		<div class="row">
-			<div class="col-sm-10 px-5 py-3 text-break">
+			<div class="col-sm-10 px-5 ml-1 py-3 text-break">
 				{{ messageData.content }}
 			</div>
 			<div class="col-sm-1 m-auto fw-bold fs-3 text-right text-white" id="score">
@@ -14,8 +16,9 @@
 			</div>
 		</div>
 
-		<div class="d-flex align-items-start mx-4">
-				<MessageIcon id="message-icon"/><span class="fw-bold text-white"> {{numMessages}}</span>
+		<div class="d-flex align-items-start mx-4 mt-1">
+			<MessageIcon id="message-icon"/>
+			<span class="fw-bold text-white"> {{ numMessages }}</span>
 		</div>
 	</div>
 </template>
@@ -23,25 +26,27 @@
 <script>
 import Clock from '../assets/Clock'
 import MessageIcon from '../assets/MessageIcon'
+import StarIcon from '../assets/StarIcon'
 
 export default {
 	name: 'OpeningMessage',
-	components: { Clock, MessageIcon },
+	components: { StarIcon, Clock, MessageIcon },
 	props: {
 		messageData: Object,
-		numMessages: Number
+		numMessages: Number,
+		writerIdInThread: Number
 	}
 }
 </script>
 
 <style scoped>
-#message{
+#message {
 	width: 800px;
 	background-color: #2e2e2e;
-	color:#8ed1c6;
+	color: #8ed1c6;
 }
 
-#clock-icon{
+#clock-icon {
 	margin-top: 0.3em;
 	margin-right: 0.5em;
 	width: 0.7em;
@@ -49,7 +54,7 @@ export default {
 	fill: #FFF;
 }
 
-#message-icon{
+#message-icon {
 	margin-right: 1em;
 	fill: #FFF;
 }
@@ -57,6 +62,15 @@ export default {
 #score {
 	text-align: right;
 	padding-right: 1em;
+}
+
+#star-icon {
+	float: left;
+	width: 1.5em;
+	height: 1.5em;
+	margin-left: -0.5em;
+	margin-top: -0.5em;
+	fill: #8ed1c6;
 }
 
 </style>
