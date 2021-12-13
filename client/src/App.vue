8a834c64a7589a229a7c1c2116f16325d43fcd16
samihuo <samihuoppila@gmail.com>
2021-12-13 13:06:44 +0200

    Metan päivitys mobiiliskaalausta varten

--- client/src/App.vue
+++ client/src/App.vue
@@ -14,7 +14,7 @@ export default {
 	head: {
 		meta: [
 			{ charset: 'utf-8' },
-			{ name: 'viewport', content: 'width=800' }
+			{ name: 'viewport', content: 'width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0' }
 		]
 	},
 	components: {
